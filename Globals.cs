8a2ec79c6a72ddcb0cc0d2f549e63cc5dec9ea24
Leonardo Bitencourt <leonardobitencourt2020@gmail.com>
2023-10-24 20:24:30 -0300

    trabalho concluído

---
+++ Globals.cs
@@ -0,0 +1,9 @@
+namespace projeto_de_redes
+{
+    public static class Globals
+    {
+        public const int NUMERO_NODOS_INTERMEDIARIOS = 7; // (10.0 + 3.0) / 2 -> Ceiling
+        public const int NUMERO_NODOS_ENTRADA = 10; 
+        public const double TAXA_APRENDIZAGEM = 0.28;
+    }
+}
---
+++ dtos/FutebolDTO.cs
@@ -0,0 +1,34 @@
+namespace ProjetoDeRedes.dtos
+{
+    internal class FutebolDTO
+    {
+
+        public int Id { get; set; }
+        
+        public string? FTR { get; set; }
+
+        public double HS { get; set; }
+
+        public double AS { get; set; }
+
+        public double HST { get; set; }
+
+        public double AST { get; set; }
+
+        public double HC { get; set; }
+
+        public double AC { get; set; } 
+
+        public double HF { get; set; }  
+
+        public double AF { get; set; }
+
+        public double HY { get; set; }
+
+        public double AY { get; set; }
+
+    }
+
+}
+
+
